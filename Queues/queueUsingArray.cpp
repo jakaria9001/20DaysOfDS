53a2511ac98fa51984c12fd20a9877f68ec5faea
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2021-04-14 00:28:49 +0530

    Update queueUsingArray.cpp

--- Queues/queueUsingArray.cpp
+++ Queues/queueUsingArray.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,34 +5,37 @@ using namespace std;
 const int SIZE = 100;
 int QUE[SIZE];    
 int front = -1, rear = -1;
-
+bool isempty(){
+    if(front==-1 && rear==-1)
+        return true;
+    return false;
+}
 void enqueue(int x){
     if(rear>=SIZE){
         cout<<"Overflow";
         return;
     }
-    if(rear==-1) front++;
-    QUE[++rear] = x;
+    
+    if(isempty()){ rear = 0, front=0;}
+    else rear++;
+    
+    QUE[rear] = x;
     cout<<"Enqueue Successful"<<endl;
 }
 void dequeue(){
-    front++;
-    if(front > rear || front==-1){
-        cout<<"Queue Empty";
-        return;
-    }    
+    if(isempty()) return;
+    if(front == rear ){
+        front = rear = -1;
+    }else front++;    
     
     cout<<"Dequeue Successful"<<endl;
 }
 int frontQ(){
-    if(front<rear) cout<<QUE[front]<<" ";
+    if(!isempty()) cout<<QUE[front]<<" ";
     else cout<<"Empty";
     // cout<<endl;
 }
-bool isempty(){
-    if(front>rear || rear==-1) return true;
-    return false;
-}
+
 void printQ(){
     if(isempty()) return;
     for(int i=front;i<=rear;i++)
@@ -72,30 +68,3 @@ int main()
     }
     return 0;
 }
-/*
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-1                                                                                                                                                                
-Enter value to be Enqueued: 5                                                                                                                                    
-Enqueue Successful                                                                                                                                               
-5                                                                                                                                                                
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-2                                                                                                                                                                
-Queue Empty                                                                                                                                                      
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-1                                                                                                                                                                
-Enter value to be Enqueued: 8                                                                                                                                    
-Enqueue Successful                                                                                                                                               
-8                                                                                                                                                                
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-2                                                                                                                                                                
-Queue Empty                                                                                                                                                      
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-2                                                                                                                                                                
-Queue Empty                                                                                                                                                      
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-3                                                                                                                                                                
-Empty                                                                                                                                                            
-                                                                                                                                                                 
-Enter 1.Enqueue, 2.Dequeue, 3.Front, 4.Empty, 0.Exit:                                                                                                            
-0    
-*/
